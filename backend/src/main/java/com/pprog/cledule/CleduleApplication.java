7206b7211bf1b8600bbfbcb2f20bbbafc2620d07
Pedro Paulo <pedropaulo062011@gmail.com>
2025-08-11 13:48:30 -0300

    :tada: Primeiro commit

---
+++ backend/src/main/java/com/pprog/cledule/CleduleApplication.java
@@ -0,0 +1,13 @@
+package com.pprog.cledule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleduleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pprog/cledule/CleduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pprog.cledule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
