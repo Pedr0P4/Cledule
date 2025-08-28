1d0a82bd038df650f5d4854327ea0f34b0fa2dad
Pedro Paulo <pedropaulo062011@gmail.com>
2025-08-27 23:28:11 -0300

    :test_tube: test(backend): Adição das novas classes no teste

--- backend/src/main/java/com/pprog/cledule/config/TestConfig.java
+++ backend/src/main/java/com/pprog/cledule/config/TestConfig.java
@@ -28,21 +28,37 @@ public class TestConfig implements CommandLineRunner {
     private DivisionRepository divisionRepository;
     @Autowired
     private EquipmentRepository equipmentRepository;
+    @Autowired
+    private DepartmentAdminRepository departmentAdminRepository;
+    @Autowired
+    private DivisionAdminRepository divisionAdminRepository;
+    @Autowired
+    private GeneralAdminRepository generalAdminRepository;
 
     @Override
     public void run(String... args) throws Exception {
+
+        GeneralAdmin gna1 = new GeneralAdmin("Denise Karen", "denikaren@email.com", "112233445");
+        DivisionAdmin dva1 = new DivisionAdmin("Geraldo Bubon", "geraldin@liame.moc", "12365498");
+        DepartmentAdmin dpa1 = new DepartmentAdmin("Bob Herbert", "bobh@email.com", "12345678");
+        DepartmentAdmin dpa2 = new DepartmentAdmin("Nuna Filho", "nunaf@email.com", "87654321");
+
+        generalAdminRepository.save(gna1);
+        divisionAdminRepository.save(dva1);
+        departmentAdminRepository.save(dpa1);
+        departmentAdminRepository.save(dpa2);
 
         SpaceUser su1 = new SpaceUser("Pedro Paulo", Role.INSTRUCTOR, "pedro@gmail.com");
         SpaceUser su2 = new SpaceUser("Cecília Cerqueira", Role.TUTOR, "cecilia@gmail.com");
 
         classUserRepository.saveAll(Arrays.asList(su1, su2));
 
-        Division dv1 = new Division("Division 1");
+        Division dv1 = new Division("Division 1", gna1);
 
         divisionRepository.save(dv1);
 
-        Department dp1 = new Department("Departamento 1", dv1);
-        Department dp2 = new Department("Departamento 2", dv1);
+        Department dp1 = new Department("Departamento 1", dv1, dva1);
+        Department dp2 = new Department("Departamento 2", dv1, dva1);
 
         departmentRepository.saveAll(Arrays.asList(dp1, dp2));
 
@@ -51,10 +67,10 @@ public class TestConfig implements CommandLineRunner {
 
         divisionRepository.save(dv1);
 
-        Space s1 = new Space("Sala 1", 40, 1, dp1);
-        Space s2 = new Space("Sala 2", 20, 2, dp2);
-        Space s3 = new Space("Auditorio 1", 120, 3, dp1);
-        Space s4 = new Space("Auditorio 2", 80, 3, dp2);
+        Space s1 = new Space("Sala 1", 40, 1, dp1, dpa1);
+        Space s2 = new Space("Sala 2", 20, 2, dp2, dpa2);
+        Space s3 = new Space("Auditorio 1", 120, 3, dp1, dpa1);
+        Space s4 = new Space("Auditorio 2", 80, 3, dp2, dpa2);
 
         spaceRepository.saveAll(Arrays.asList(s1, s2, s3, s4));
 
@@ -72,10 +88,10 @@ public class TestConfig implements CommandLineRunner {
 
         equipmentRepository.saveAll(Arrays.asList(e1, e2, e3, e4));
 
-        Reservation r1 = new Reservation("Aula de LP1", LocalDateTime.now(), LocalDateTime.of(2025, 8, 20, 14, 0), ReservationStatus.WAITING, su1, s2);
-        Reservation r2 = new Reservation("Aula de Histologia", LocalDateTime.now(), LocalDateTime.of(2025, 8, 22, 16, 0), ReservationStatus.WAITING, su2, s1);
-        Reservation r3 = new Reservation("Aula de APOO", LocalDateTime.now(), LocalDateTime.of(2025, 8, 19, 12, 0), ReservationStatus.WAITING, su1, s4);
-        Reservation r4 = new Reservation("Aula de Anatomia", LocalDateTime.now(), LocalDateTime.of(2025, 8, 24, 7, 30), ReservationStatus.WAITING, su2, s3);
+        Reservation r1 = new Reservation("Aula de LP1", LocalDateTime.now(), LocalDateTime.of(2025, 8, 20, 14, 0), ReservationStatus.WAITING, su1, s2, dpa1);
+        Reservation r2 = new Reservation("Aula de Histologia", LocalDateTime.now(), LocalDateTime.of(2025, 8, 22, 16, 0), ReservationStatus.WAITING, su2, s1, dpa1);
+        Reservation r3 = new Reservation("Aula de APOO", LocalDateTime.now(), LocalDateTime.of(2025, 8, 19, 12, 0), ReservationStatus.WAITING, su1, s4, dpa1);
+        Reservation r4 = new Reservation("Aula de Anatomia", LocalDateTime.now(), LocalDateTime.of(2025, 8, 24, 7, 30), ReservationStatus.WAITING, su2, s3, dpa2);
 
         reservationRepository.saveAll(Arrays.asList(r1, r2, r3, r4));
 
